2372454e5162d8bcbfcb821d7f8dd902192c2309
Rick <53375860+modularsamples@users.noreply.github.com>
2020-06-28 02:07:29 +0100

    sh2

---
+++ WebsiteDemos/patches/file8.js
@@ -0,0 +1,20 @@
+
+var keys = new Tone.Sampler({
+		"C1" : "FILE1.[mp3|ogg]",
+			"E1" : "FILE5.[mp3|ogg]",
+			"G#1" : "FILE9.[mp3|ogg]",
+			"C2" : "FILE13.[mp3|ogg]",
+			"E2" : "FILE17.[mp3|ogg]",
+			"G#2" : "FILE21.[mp3|ogg]",
+			"C3" : "FILE25.[mp3|ogg]"
+				
+				
+				
+					}, {
+
+						"baseUrl" : "https://raw.githubusercontent.com/publicsamples/Roland-SH-2/master/MP3/Plastics/"
+					})					.connect(filter);
+					
+					
+					
+			
